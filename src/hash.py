d1bfcabd10fb30a052188a37af73788989ddf151
jneira <johnny.neira@gmail.com>
2024-07-30 08:30:05 +0200

    Améliore la sécurité dans create_sha256_hash en supprimant le mot de passe après hashing
    Mette à jour la méthode create_sha256_hash pour renforcer la sécurité en supprimant le mot de passe original de l'instance après sa conversion en hash SHA-256. Cette modification empêche l'accès ultérieur au mot de passe en clair. La documentation de la méthode a également été mise à jour pour refléter ce changement et fournir une explication détaillée du processus de hashing et de la suppression du mot de passe.

--- src/hash.py
+++ src/hash.py
@@ -14,13 +14,18 @@ class SecureHasher:
 
     def create_sha256_hash(self) -> str:
         """
-        Génère un hash SHA-256 à partir du mot de passe stocké dans l'instance.
-        Cette méthode encode d'abord le mot de passe en bytes, puis utilise l'algorithme SHA-256 pour
-        produire un hash du mot de passe encodé. Le hash résultant est retourné sous forme de chaîne
-        hexadécimale.
+        Génère un hash SHA-256 sécurisé à partir du mot de passe stocké dans l'instance et supprime le mot
+        de passe original.
+
+        Cette méthode commence par encoder le mot de passe en bytes, puis utilise l'algorithme SHA-256 pour
+        produire un hash du mot de passe encodé. Après la génération du hash, le mot de passe original est
+        supprimé de l'instance pour renforcer la sécurité. Le hash résultant est retourné sous forme de
+        chaîne hexadécimale.
 
         :return: Le hash SHA-256 du mot de passe, sous forme de chaîne hexadécimale.
         """
+
         encoded_data = self._password.encode()
         hashed_data = hashlib.sha256(encoded_data).hexdigest()
+        del self._password
         return hashed_data
